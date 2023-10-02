91d2a30580df462f0fc600b4451d1df14558fc26
titapuspitas <141605952+titapuspitas@users.noreply.github.com>
2023-10-02 15:41:18 +0700

    Add files via upload

---
+++ tita/javascript/latihan1.js
@@ -0,0 +1,13 @@
+document.getElementById("text").innerHTML = "MY BIODATA"
+
+let nama = "Nama : Tita puspita sari";
+document.getElementById("nama").innerHTML = nama;
+
+let alamat = "Alamat : kp.cidahu RT.01 RW.03";
+document.getElementById("alamat").innerHTML = alamat;
+
+let ttg = "Tempat Tanggal Lahir : Tasikmalaya 06 september 2006";
+document.getElementById("ttg").innerHTML = ttg;
+
+let hoby = "Hoby : dengerin musik";
+document.getElementById("hoby").innerHTML = hoby;
---
+++ tita/javascript/latihan2.js
@@ -0,0 +1,4 @@
+let nilai = 5 + 3;
+document.writeln("<p>5 + 3 = " + nilai + "</p>").innerHTML = nilai;
+
+
